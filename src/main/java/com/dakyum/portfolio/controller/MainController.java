90bfa1f2e73c969cb7d2c300fa53079b4079f328
dakyumlee <oicrcutie@gmail.com>
2025-10-06 18:23:51 +0900

    Add login endpoint

--- src/main/java/com/dakyum/portfolio/controller/MainController.java
+++ src/main/java/com/dakyum/portfolio/controller/MainController.java
@@ -31,6 +31,11 @@ public class MainController {
         return "index";
     }
     
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+    }
+    
     @GetMapping("/api/project/{id}")
     @ResponseBody
     public Project getProject(@PathVariable Long id) {
